53dabeac42fed3ee31b67375e9a47ad9e535857d
Rahul80010 <rahulmandal8001095@gmail.com>
2026-09-12 14:48:23 +0530

    fix: correct authMiddleware path in hotelBookingRoutes to prevent startup crash

--- routes/hotelBookingRoutes.js
+++ routes/hotelBookingRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const hotelBookingController = require('../controllers/hotelBookingController');
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/check-in', protect, hotelBookingController.checkInGuest);
 router.get('/active', protect, hotelBookingController.getActiveBookings);
